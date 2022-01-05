76be7e968339f1c51a4f8c02f0897ebfab2f67b0
AndrewSinukov <andrew.sinukov.it@gmail.com>
2022-01-05 13:42:05 +0200

    added platform layer

--- platform/variables.tf
+++ platform/variables.tf
@@ -4,7 +4,6 @@ variable "region" {
 
 variable "remote_state_bucket" {}
 variable "remote_state_key" {}
-
 variable "ecs_cluster_name" {}
 variable "internet_cidr_blocks" {}
 variable "ecs_domain_name" {}
